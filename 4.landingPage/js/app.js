cf64b521153dc78d1496a35a93a08fe744c0fb45
Carlos Castillo Meneses <65776444+carloscastillom@users.noreply.github.com>
2022-05-19 09:10:57 -0500

    Add files via upload

---
+++ 4.landingPage/js/app.js
@@ -0,0 +1,73 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const navBar = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
+console.log(sections);
+for (let section of sections) {
+    const header = section.querySelector('h2')
+    const item = document.createElement('p');
+    item.textContent = header.textContent;
+    navBar.appendChild(item);
+}
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
