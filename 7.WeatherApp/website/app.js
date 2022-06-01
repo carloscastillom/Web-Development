dfb8ba3854f634f352f8bdb1301867807f04842e
Carlos Castillo Meneses <65776444+carloscastillom@users.noreply.github.com>
2022-06-01 10:17:54 -0500

    Add files via upload

--- 7.WeatherApp/website/app.js
+++ 7.WeatherApp/website/app.js
@@ -1,5 +1,41 @@
-/* Global Variables */
+// Async POST
+const postData = async ( url = '', data = {})=>{
+    console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+// Async GET
+const retrieveData = async (url='') =>{ 
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+function postGet(){
+  postData('/animal', {fav:'lion'})
+    .then(function(data){
+      retrieveData('/all')
+    })
+}
+
+postGet()
